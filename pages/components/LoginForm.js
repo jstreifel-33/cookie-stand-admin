6dfbbd7e6d90b78d3a11152afa57d9086c53fb49
Joe Streifel <josephstreifel@gmail.com>
2022-01-24 18:38:06 -0800

    styles login page

--- pages/components/LoginForm.js
+++ pages/components/LoginForm.js
@@ -4,14 +4,16 @@ export default function LoginForm({onLogin}) {
         onLogin(event.target.username.value, event.target.password.value)
     }
     return (
-        <div>
-            <h2>Please Login to Cookie Stand Admin</h2>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="username">Username:</label>
-                <input type="text" name="username" />
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" />
-                <button type="submit">Login</button>
+        <div className="flex flex-col bg-emerald-200 border border-emerald-500 border-2 w-1/2 rounded mx-auto my-4">
+            <form onSubmit={handleSubmit} className="flex flex-col justify-center text-center px-8">
+
+                <label htmlFor="username" className="my-1 font-bold">USER NAME</label>
+                <input type="text" name="username" placeholder="User Name" className="my-2" />
+
+                <label htmlFor="password" className="my-1 font-bold">PASSWORD</label>
+                <input type="password" name="password" placeholder="password" className="my-2" />
+
+                <button type="submit" className="bg-emerald-500 rounded p-2 my-4">SIGN IN</button>
             </form>
         </div>
     )

6b41d17ebe6b8bd969ad74be812530d281277cc2
Joe Streifel <josephstreifel@gmail.com>
2022-01-24 19:24:15 -0800

    styling

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer(props) {
     return (
-        <footer className='bg-emerald-500 p-4'>
+        <footer className='bg-emerald-500 p-4 text-center'>
             <p>{props.storeCount} Locations World Wide</p>
         </footer>
     )
